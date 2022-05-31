a3f5a51584c33baa871fa69a2a7599c27ae1d81b
Wangxiuzhi <976524161@qq.com>
2022-05-31 22:50:59 +0800

    Initial Commit

---
+++ MyChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyChat
+//
+//  Created by 王修智 on 2022/5/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
